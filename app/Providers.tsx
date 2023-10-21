135f5efe9ca297956c9833da37c0861143594be9
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2023-10-21 10:57:35 +0800

    task1 - add query client

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,17 +1,20 @@
 'use client';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
 import { PropsWithChildren } from 'react';
 import { Toaster } from 'sonner';
 
+const queryClient = new QueryClient();
+
 export const Providers = ({ children }: PropsWithChildren) => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         <Toaster />
         <SessionProvider>{children}</SessionProvider>
       </ThemeProvider>
-    </>
+    </QueryClientProvider>
   );
 };
