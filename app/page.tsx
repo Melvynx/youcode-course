1768d77a5eaf9f66b82a9d84f36e98ca6aab87f4
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2023-11-06 15:22:41 +0800

    push my course

--- app/page.tsx
+++ app/page.tsx
@@ -83,7 +83,7 @@ export default function Home() {
           <iframe
             width="560"
             height="315"
-            src="https://www.youtube-nocookie.com/embed/taw7AYTN0ds?si=i6Fy0ojXwEQnyBtz&amp;controls=0"
+            src="https://www.youtube-nocookie.com/embed/TsVFEtj_Jrw?si=CfbBjfbkhG6i210d"
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           ></iframe>
